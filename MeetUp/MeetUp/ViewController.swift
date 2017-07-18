1a5d13e3d827943cf13888ad07d07d079cb760fb
Sajal Kherde <sajalkherde@Sajals-Mac.local>
2017-07-17 22:36:17 -0500

    New Files

---
+++ MeetUp/MeetUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeetUp
+//
+//  Created by Sajal Kherde on 7/17/17.
+//  Copyright © 2017 SJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Welcome to GitHub Pages
-##ITS ME
+##ITS SAjal
 
 You can use the [editor on GitHub](https://github.com/sajalkherde/MeetUp/edit/master/README.md) to maintain and preview the content for your website in Markdown files.
 
